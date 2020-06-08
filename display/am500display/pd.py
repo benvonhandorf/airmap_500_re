e98b183c82f40e589941f1b563b4ced7cb33aa50
Ben Von Handorf <developer@benvonhandorf.com>
2020-06-08 18:51:24 -0400

    WIP

--- display/am500display/pd.py
+++ display/am500display/pd.py
@@ -1,8 +1,9 @@
 import sigrokdecode as srd
 import math
+from PIL import Image
 
 class Decoder(srd.Decoder):
-	api_version = 3
+	api_version = 2
 	id = 'am500display'
 	name = 'am500 display protocol'
 	longname = 'AirMap 500 Display Protocol'
@@ -30,27 +31,60 @@ class Decoder(srd.Decoder):
 
 	def start(self):
 		self.out_ann = self.register(srd.OUTPUT_ANN)
-
-
-	def reset(self):
 		self.sync = False
+		self.pc = 0
+		self.width = 180
+		self.height = 240
+		self.skip = 0
+		self.image = Image.new('L', (self.width, self.height))
+		self.x = 0
+		self.y = 0
+		self.pixel_values = [0x00, 0x0F, 0x40, 0xFF]
+
+	def write_next_pixel(self, value):
+		self.image.putpixel((self.x, self.y), self.pixel_values[int(value)])
+		
+		self.x = self.x + 1
+
+		if self.x >= self.width:
+			self.y = self.y + 1
+			self.x = 0
+			self.image.save('image.jpg')
+
+		if self.y >= self.height:
+			print('Image wrapped')
+
+			self.y = 0
+			self.x = 0
+
+	def handle_value(self, value):
+		self.put(ss, es, self.out_ann, [0, ['{} {:02X}'.format(value, value)]])
+
+		if self.skip == 0:
+			self.write_next_pixel(value)
+		else:
+			self.skip = self.skip - 1
+
+
 
 	def decode(self, ss, es, data):
 		
 		for (self.samplenum, pins) in data:
 			(pc, sp, d0, d1, d2, d3) = pins
 
-			if sp == 1:
+			if self.sync == False and sp == 1:
 				self.put(ss, es, self.out_ann, [1, ['Sync']])
 				self.sync = True
 
 			if self.sync and self.pc == 0 and pc == 1:
 				# PC went positive, time to sample
-				value = d0 + d1 << 1 + d2 << 2 + d3 << 3
+				value = d0 + (d1 << 1)
 
-				self.put(ss, es, self.out_ann, [0, [value, '{:02X}'.format(value)]])
+				self.handle_value(value)
 
-				print("value: {}".format(value))
+				value = d2 + (d3 << 1)
+
+				self.handle_value(value)
 
 			self.pc = pc
 
